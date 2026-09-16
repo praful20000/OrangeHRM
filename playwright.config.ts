9d4a1c2a4ee3eb23e5b82c0e47df75bd0c43d5c5
Prafulla Mahajan <mahajanpraful5@gmail.com>
2026-09-16 20:01:28 +0530

    config change

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,8 +14,8 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* The public demo account requires isolated sequential sessions. */
-  fullyParallel: false,
-  workers: 1,
+  fullyParallel: true,
+  workers: 4,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -44,15 +44,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
     // {
